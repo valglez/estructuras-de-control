23814f4f7511c5731825033fdd90b93704744d1c
valglez <vsalglez@gmail.com>
2020-10-06 18:29:52 +0200

    Hemos añadido mas líneas al código

--- Condicionales/if-elif-else.py
+++ Condicionales/if-elif-else.py
@@ -2,7 +2,13 @@ edad = int(input('Escriba su edad: '))
 if edad < 18:
     print('Usted es menor de edad')
     print('Usted aún no puede conducir un coche')
+    miNombre = input('Dígame su nombre')
+    print(f'{miNombre}, aún debe esperar ' f'{18 - edad} años para ser mayor de edad')
+    print('Póngase en contacto con nosotros en un futuro')
+
 elif edad >= 18:
     print('Usted es mayor de edad')
     print('Usted si puede conducir un coche')
-print(f'Usted tiene {edad} años')
+    miNombre = input('Dígame su nombre')
+    print(f'{miNombre} usted ya tiene ' f'{edad} años')
+    print('Le voy a enviar un email para que pueda inscribirse en nuestra academia')
--- Condicionales/if-elif-else2.py
+++ Condicionales/if-elif-else2.py
@@ -1,5 +1,3 @@
-#Usamos los condicionales para mezclar colores
-
 print('Este programa mezcla dos colores.')
 print('  r. Rojo      a. Azul')
 primera = input('  Elija un color (r o a): ')
@@ -10,6 +8,8 @@ if primera == 'r':
         print('La mezcla de Rojo y Amarillo produce Naranja')
     elif segunda == 'v':
         print('La mezcla de Rojo y Verde produce Marrón')
+    else:
+        print('No disponemos de ese color en nuestra base de datos')
 if primera == 'a':
     print('  am. Amarillo     v. Verde')
     segunda = input('  Elije un color (am o v): ')
@@ -17,4 +17,7 @@ if primera == 'a':
         print('La mezcla de Azul y Amarillo produce Verde')
     elif segunda == 'v':
         print('La mezcla de Azul y Verde produce Turquesa')
-        
+    else:
+        print('No disponemos de ese color en nuestra base de datos')
+else:
+    print('No disponemos de ese color en nuestra base de datos')
