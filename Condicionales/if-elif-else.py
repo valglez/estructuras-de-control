0811245bc913dece5024fd93a44a8c35d255f544
valglez <vsalglez@gmail.com>
2020-10-06 16:16:36 +0200

    Revert "Creamos una rama para los condicionales"
    This reverts commit cc079c9de3b9cb224c62cafca779a44d8c6b9aab.

--- Condicionales/if-elif-else.py
+++
@@ -1,8 +0,0 @@
-edad = int(input('Escriba su edad: '))
-if edad < 18:
-    print('Usted es menor de edad')
-    print('Usted aún no puede conducir un coche')
-elif edad >= 18:
-    print('Usted es mayor de edad')
-    print('Usted si puede conducir un coche')
-print(f'Usted tiene {edad} años')
--- README.md
+++ README.md
@@ -5,3 +5,4 @@ En esta ocasión vamos a hacer uso de
 * Condicionales
 * Bucles
     
+Cada tipo de estructura de control tendrá su propia rama, y estas ramas se subdividirán en otras ramas por cada tipo de condicionales y bucles que correspondan
