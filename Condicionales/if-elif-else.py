18141458dde41eea62c0a0da571f6c5ae4f8eb11
valglez <vsalglez@gmail.com>
2020-10-06 18:58:32 +0200

    Rama de condicionales finalizada

--- Condicionales/if-elif-else.py
+++ Condicionales/if-elif-else.py
@@ -3,12 +3,12 @@ if edad < 18:
     print('Usted es menor de edad')
     print('Usted aún no puede conducir un coche')
     miNombre = input('Dígame su nombre')
-    print(f'{miNombre}, aún debe esperar ' f'{18 - edad} años para ser mayor de edad')
+    print(f'{miNombre}, aún debe esperar {18 - edad} años para ser mayor de edad')
     print('Póngase en contacto con nosotros en un futuro')
 
 elif edad >= 18:
     print('Usted es mayor de edad')
     print('Usted si puede conducir un coche')
     miNombre = input('Dígame su nombre')
-    print(f'{miNombre} usted ya tiene ' f'{edad} años')
+    print(f'{miNombre} usted ya tiene {edad} años')
     print('Le voy a enviar un email para que pueda inscribirse en nuestra academia')
