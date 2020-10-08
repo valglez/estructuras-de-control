78ab3b0df8e242a2bcda240e2e58c9d070fa41a5
valglez <vsalglez@gmail.com>
2020-10-08 11:37:45 +0200

    Actualizamos for.py y creamos range.py

---
+++ Bucles/for.py
@@ -0,0 +1,6 @@
+i = 10
+print(f'Antes del bucle la i vale {i}')
+for i in [0, 1, 2, 3]:
+    print(f' {i} * {i} = {i**2}')
+print(f'Después del bucle la i vale {i}')
+print('Final')
---
+++ Bucles/range.py
@@ -0,0 +1,8 @@
+frase = input('Este es un programa motivacional. Dime tu objetivo: ')
+veces = int(input('Perfecto. Ahora dime cuántas veces quiere que te lo repita: '))
+
+print()
+for i in range(veces):
+    print(f'{frase}')
+print()
+print('No desistas')
